79752fb572d7ff7ba77255ce0ba48310adba4c73
Abhay-Gupta-Git <guptaabhay2618@gmail.com>
2026-04-15 12:04:29 +0530

    server created

---
+++ software/app.js
@@ -0,0 +1,14 @@
+ const express =require('express');
+ const app=express();
+ const path=require('path');
+
+app.set('view engine')
+app.set('views',path.join(__dirname,'views'));
+
+//public folder 
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.listen(8080 , ()=>{
+    console.log("server connected at port 8080")
+})
